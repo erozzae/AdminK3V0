256ce8f5ad344d939be58569eae41965af1aca23
marisadianp <93934909+marisadianp@users.noreply.github.com>
2022-05-27 00:07:32 +0700

    Update sidebar.blade.php

--- resources/views/layout/backend/sidebar.blade.php
+++ resources/views/layout/backend/sidebar.blade.php
@@ -8,9 +8,6 @@
         <div class="sidebar-brand-text mx-3">K3V0</div>
     </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-
     <!-- Nav Item - Dashboard -->
     @can('admin')
     <li class="nav-item">
@@ -25,30 +22,13 @@
         <a class="nav-link" href="{{ route('user') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
-    </li>  
-    @elseCan('admin')
-    <li class="nav-item">
+    </li>
+    <!-- <li class="nav-item">
         <a class="nav-link" href="{{ route('user') }}">
             <i class="fas fa-fw fa-user"></i>
             <span>User Dashboard</span></a>
-    </li>
+    </li> -->
     @endCan
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
-    </div>
 
     @can('admin')
     <li class="nav-item">
@@ -58,9 +38,7 @@
             <span>User</span></a>
     </li>
 
-
-
-    {{-- <li class="nav-item">
+    <!-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
             aria-expanded="true" aria-controls="collapseOne">
             <i class="fas fa-fw fa-table"></i>
@@ -71,11 +49,11 @@
                 <a class="collapse-item" href="{{ route('user.index') }}">User</a>
             </div>
         </div>
-    </li> --}}
+    </li> -->
     @endcan
     
     <li class="nav-item">
-        <a class="nav-link" href="">
+        <a class="nav-link" href="{{ route('blank-page') }}">
             <i class="fa fa-exclamation-circle"></i>
             <span>About</span>
         </a>
@@ -83,28 +61,71 @@
 
     @can('admin')
     <li class="nav-item">
-        <a class="nav-link" href="">
-            <i class=" fa fa-align-center"></i>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMateri"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fa fa-align-center"></i>
             <span>Materi</span>
         </a>
+        <div id="collapseMateri" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class=" py-2 collapse-inner rounded">
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB I</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB II</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB III</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB IV</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB V</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB VI</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB VII</span>
+                </a>
+                <a class="collapse-item mb-1 bg-transparent" href="">
+                    <span class="text-white">BAB VIII</span>
+                </a>
+                <div class="collapse-divider"></div>
+            </div>
+        </div>
     </li>
     @endcan
    
     @can('admin')
     <li class="nav-item">
-        <a class="nav-link" href="">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSoal"
+            aria-expanded="true" aria-controls="collapsePages">
             <i class="fa fa-check-square"></i>
             <span>Soal</span>
         </a>
+        <div id="collapseSoal" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class=" py-2 collapse-inner rounded">
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB I</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB II</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB III</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB IV</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB V</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB VI</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB VII</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href=""><span class="text-white">BAB VIII</span></a>
+                <div class="collapse-divider"></div>
+            </div>
+        </div>
     </li>
     @endcan
 
-    {{-- <li class="nav-item">
+    <!-- <li class="nav-item">
         <a class="nav-link" href="">
             <i class="fas fa-file-signature"></i>
             <span>Regulasi</span></a>
-    </li> --}}
-
+    </li> -->
 
     <!-- Divider -->
     <hr class="sidebar-divider">
@@ -130,43 +151,30 @@
                 <div class="collapse-divider"></div>
                 <h6 class="collapse-header">Other Pages:</h6>
                 <a class="collapse-item mb-1 bg-transparent" href="{{ route('404-page') }}"><span class="text-white">404 Page</span></a>
-                <a class="collapse-item mb-1 bg-transparent" href="{{ route('blank-page') }}"><span class="text-white">Blank Page</span></a>
             </div>
         </div>
     </li>
-    
 
     <!-- Nav Item - Charts -->
-    {{-- <li class="nav-item">
+    <!-- <li class="nav-item">
         <a class="nav-link" href="{{ route('chart') }}">
             <i class="fas fa-fw fa-chart-area"></i>
             <span>Charts</span></a>
-    </li> --}}
+    </li> -->
 
     <!-- Nav Item - Tables -->
-    {{-- <li class="nav-item">
+    <!-- <li class="nav-item">
         <a class="nav-link" href="{{ route('tables') }}">
             <i class="fas fa-fw fa-table"></i>
             <span>Tables</span></a>
-    </li> --}} 
+    </li> -->
     {{-- ->menuju admin tables di route --}}
 
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('profile') }}">
-            <i class="fas fa-fw fa-user"></i>
-            <span>Profile</span></a>
-    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
 
-    
-    @can('admin')
-    <li class="nav-item">
-        <a class="nav-link" href="">
-            <i class="fa fa-clipboard"></i>
-            <span> User Manual</span>
-        </a>
-    </li>
-    @endcan
-
-    
-    
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 </ul>
