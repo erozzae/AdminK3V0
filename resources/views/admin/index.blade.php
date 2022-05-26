4896720333743fd32fc664d598610b4c9db1044b
erozzae <rosihan14@mail.ugm.ac.id>
2022-05-26 20:33:54 +0700

    K3V0 Admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layout.backend.app',[
+    'title' => 'Dashboard',
+    'pageTitle' => 'Dashboard'
+])
+@section('content')
+<!-- Content Row -->
+<div class="row">
+     <!-- Users -->
+     <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-md-3">
+                         {{-- <i class="fas fa-users fa-2x text-gray-300"></i> --}}
+                         <i><img src="{{ asset('img/icon/users.png') }}" alt="" width="50px"></i>
+                       
+                    </div>
+                    <div class="col-md-9">
+                        <div class="h4 mb-1 font-weight-bold text-gray-800 ">User</div>
+                        <div class="text-md font-weight-bold text-warning text-uppercase mb-1">8 User</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Materials -->
+    <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-md-3">
+                        <i><img src="{{ asset('img/icon/materials.png') }}" alt="" width="50px"></i>
+                   </div>
+
+                    <div class="col-md-9">
+                        <div class="h4 mb-1 font-weight-bold text-gray-800 ">Materi</div>
+                        <div class="text-md font-weight-bold text-primary text-uppercase mb-1">8 Materi</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-md-3">
+                        <i><img src="{{ asset('img/icon/tasks.png') }}" alt="" width="50px"></i>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="h4 mb-1 font-weight-bold text-gray-800 ">Soal</div>
+                        <div class="text-md font-weight-bold text-success text-uppercase mb-1">1 Soal</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+<!-- Content Row -->
+<div class="row">
+    <div class="col-lg-6 mb-4">
+
+        <!-- Illustrations -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Introduction</h6>
+            </div>
+            <div class="card-body">
+                <div class="text-center">
+                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                        src="{{ asset('template/backend/sb-admin-2') }}/img/undraw_posting_photo.svg" alt="">
+                </div>
+                <p> K3V0 merupakan aplikasi yang ditujukan kepada siswa SMK untuk mendapatkan informasi 
+                    K3 secara mudah guna mengimplementasikan K3 saat di lab dan magang industri.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6 mb-4">
+        <!-- Approach -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">About</h6>
+            </div>
+            <div class="card-body">
+                <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
+                    CSS bloat and poor page performance. Custom CSS classes are used to create
+                    custom components and custom utility classes.</p>
+                <p class="mb-0">Before working with this theme, you should become familiar with the
+                    Bootstrap framework, especially the utility classes.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
---
+++ resources/views/layout/backend/sidebar.blade.php
@@ -0,0 +1,172 @@
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+        <div class="sidebar-brand-icon">
+            <i class="fa fa-plus-square"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">K3V0</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    @can('admin')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('admin') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+    @endcan
+
+    @can('user')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('user') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>  
+    @elseCan('admin')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('user') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>User Dashboard</span></a>
+    </li>
+    @endCan
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Interface
+    </div>
+
+    @can('admin')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('user.index') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <!-- <i class="fas fa-fw fa-table"></i> -->
+            <span>User</span></a>
+    </li>
+
+
+
+    {{-- <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
+            aria-expanded="true" aria-controls="collapseOne">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Master Data</span>
+        </a>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('user.index') }}">User</a>
+            </div>
+        </div>
+    </li> --}}
+    @endcan
+    
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fa fa-exclamation-circle"></i>
+            <span>About</span>
+        </a>
+    </li>
+
+    @can('admin')
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class=" fa fa-align-center"></i>
+            <span>Materi</span>
+        </a>
+    </li>
+    @endcan
+   
+    @can('admin')
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fa fa-check-square"></i>
+            <span>Soal</span>
+        </a>
+    </li>
+    @endcan
+
+    {{-- <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-file-signature"></i>
+            <span>Regulasi</span></a>
+    </li> --}}
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Addons
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Pages</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class=" py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Login Screens:</h6>
+                <a class="collapse-item mb-1 bg-transparent" href="{{ route('login') }}"><span class="text-white">Login</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href="{{ route('register') }}"><span class="text-white">Register</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href="{{ route('forgot-password') }}"><span class="text-white">Forgot Password</span></a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">Other Pages:</h6>
+                <a class="collapse-item mb-1 bg-transparent" href="{{ route('404-page') }}"><span class="text-white">404 Page</span></a>
+                <a class="collapse-item mb-1 bg-transparent" href="{{ route('blank-page') }}"><span class="text-white">Blank Page</span></a>
+            </div>
+        </div>
+    </li>
+    
+
+    <!-- Nav Item - Charts -->
+    {{-- <li class="nav-item">
+        <a class="nav-link" href="{{ route('chart') }}">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Charts</span></a>
+    </li> --}}
+
+    <!-- Nav Item - Tables -->
+    {{-- <li class="nav-item">
+        <a class="nav-link" href="{{ route('tables') }}">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tables</span></a>
+    </li> --}} 
+    {{-- ->menuju admin tables di route --}}
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('profile') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Profile</span></a>
+    </li>
+
+    
+    @can('admin')
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fa fa-clipboard"></i>
+            <span> User Manual</span>
+        </a>
+    </li>
+    @endcan
+
+    
+    
+</ul>
